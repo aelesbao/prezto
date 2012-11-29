90b4ed541e163d7b22473a24d375fad6e6da48ec
Augusto Rocha Elesbão <aelesbao@gmail.com>
2012-11-29 17:12:37 -0200

    Added 'ignore-parents' to cd completion.

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -84,7 +84,7 @@ zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
 
 # Directories
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories ignore-parents parent pwd
 zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
 zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
 zstyle ':completion:*' squeeze-slashes true

b949c1a919d09bc3fd8dfa94add0e7e6a6e4a561
Augusto Rocha Elesbão <aelesbao@gmail.com>
2012-11-29 17:18:09 -0200

    Removed interactive mode from rm command

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -55,7 +55,6 @@ alias mv="${aliases[mv]:-mv} -i"
 alias p='${(z)PAGER}'
 alias po='popd'
 alias pu='pushd'
-alias rm="${aliases[rm]:-rm} -i"
 alias type='type -a'
 
 alias all_users="cut -d: -f1 < /etc/passwd | sort | xargs echo"

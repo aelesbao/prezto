2848e985aa1837770079b71a03cd3212d1e030cd
Augusto Elesbão <aelesbao@gmail.com>
2015-07-02 20:01:19 -0300

    Arch Linux fixes

--- modules/pacman/init.zsh
+++ modules/pacman/init.zsh
@@ -40,51 +40,51 @@ fi
 alias pac="${_pacman_frontend}"
 
 # Installs packages from repositories.
-alias paci="${_pacman_sudo}${_pacman_frontend} --sync"
+alias paci="${_pacman_sudo}${_pacman_frontend} -S"
 
 # Installs packages from files.
-alias pacI="${_pacman_sudo}${_pacman_frontend} --upgrade"
+alias pacI="${_pacman_sudo}${_pacman_frontend} -U"
 
 # Removes packages and unneeded dependencies.
-alias pacx="${_pacman_sudo}${_pacman_frontend} --remove"
+alias pacx="${_pacman_sudo}${_pacman_frontend} -R"
 
 # Removes packages, their configuration, and unneeded dependencies.
-alias pacX="${_pacman_sudo}${_pacman_frontend} --remove --nosave --recursive"
+alias pacX="${_pacman_sudo}${_pacman_frontend} -R --nosave --recursive"
 
 # Displays information about a package from the repositories.
-alias pacq="${_pacman_frontend} --sync --info"
+alias pacq="${_pacman_frontend} -S --info"
 
 # Displays information about a package from the local database.
-alias pacQ="${_pacman_frontend} --query --info"
+alias pacQ="${_pacman_frontend} -Q --info"
 
 # List the contents of the queried package
-alias pacL='${_pacman_frontend} --query -l'
+alias pacL="${_pacman_frontend} -Q -l"
 
 # Query the package that owns <file>
-alias pacO='${_pacman_frontend} --query -o'
+alias pacO="${_pacman_frontend} -Q -o"
 
 # Searches for packages in the repositories.
-alias pacs="${_pacman_frontend} --sync --search"
+alias pacs="${_pacman_frontend} -S --search"
 
 # Searches for packages in the local database.
-alias pacS="${_pacman_frontend} --query --search"
+alias pacS="${_pacman_frontend} -Q --search"
 
 # Lists orphan packages.
-alias pacman-list-orphans="${_pacman_sudo}${_pacman_frontend} --query --deps --unrequired"
+alias pacman-list-orphans="${_pacman_sudo}${_pacman_frontend} -Q --deps --unrequired"
 
 # Removes orphan packages.
-alias pacman-remove-orphans="${_pacman_sudo}${_pacman_frontend} --remove --recursive \$(${_pacman_frontend} --quiet --query --deps --unrequired)"
+alias pacman-remove-orphans="${_pacman_sudo}${_pacman_frontend} -R --recursive \$(${_pacman_frontend} --quiet -Q --deps --unrequired)"
 
 # Synchronizes the local package and Arch Build System databases against the
 # repositories.
 if (( $+commands[abs] )); then
-  alias pacu="${_pacman_sudo}${_pacman_frontend} --sync --refresh && sudo abs"
+  alias pacu="${_pacman_sudo}${_pacman_frontend} -S --refresh && sudo abs"
 else
-  alias pacu="${_pacman_sudo}${_pacman_frontend} --sync --refresh"
+  alias pacu="${_pacman_sudo}${_pacman_frontend} -S --refresh"
 fi
 
 # Synchronizes the local package database against the repositories then
 # upgrades outdated packages.
-alias pacU="${_pacman_sudo}${_pacman_frontend} --sync --refresh --sysupgrade"
+alias pacU="${_pacman_sudo}${_pacman_frontend} -S --refresh --sysupgrade"
 
 unset _pacman_{frontend,sudo}

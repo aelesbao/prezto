e2c55b36ce1c60b3e23e964d548d36a3c1ec9576
Augusto Elesbão <aelesbao@users.noreply.github.com>
2025-08-08 16:05:16 +0200

    fix(docker): remove statuses not supported by podman

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -57,11 +57,9 @@ function dkmd {
 function dkclean {
   docker ps -a --quiet \
     -f 'status=created' \
-    -f 'status=restarting' \
     -f 'status=removing' \
     -f 'status=paused' \
     -f 'status=exited' \
-    -f 'status=dead' \
     | xargs -I {} docker rm {}
   docker images | grep '<none>' | awk '{print $3}' | xargs docker rmi
   docker volume prune
